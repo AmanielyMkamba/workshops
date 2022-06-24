51f43c433b7dd636b506971e53b6248e7867821f
Amani Mkamba <amani.mkamba@gmail.com>
2022-06-24 00:07:36 -0400

    only-odds.js

--- 02-loops/01-only-odds/only-odds.js
+++ 02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+
+function onlyOdds(num) {
+
+    let sumOdds = 0;
+
+    for(let i = num; i >= 1; i--) {
+        if(i % 2 === 1) {
+            sumOdds += i;
+        }
+    }
+    return sumOdds
+}
+
+console.log(onlyOdds(6))
