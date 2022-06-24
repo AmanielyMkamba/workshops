2ffb92560d960f86868826815985a86ffa755fe1
Amani Mkamba <amani.mkamba@gmail.com>
2022-06-24 00:33:04 -0400

    crazy-caps.js

--- 02-loops/02-crazy-caps/crazy-caps.js
+++ 02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+
+function crazyCaps(sentence) {
+    let crazySentence = "";
+    for(let i = 0; i < sentence.length; i++) {
+        if(i % 2 === 0) {
+            crazySentence += sentence[i].toLowerCase()
+        }
+        else {
+            crazySentence += sentence[i].toUpperCase()
+        }
+    }
+    return crazySentence
+}
+console.log(crazyCaps("fullstack is amazing"))
