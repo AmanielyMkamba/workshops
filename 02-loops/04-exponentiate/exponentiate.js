862b1da0d6e43b5b4512c19140b5542462b12b54
Amani Mkamba <amani.mkamba@gmail.com>
2022-06-25 22:53:42 -0400

    exponentiate.js

--- 02-loops/04-exponentiate/exponentiate.js
+++ 02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function exponentiate(base, power) {
+    let results = 1;
+    for(let i = 0; i < power; i++) {
+        results *= base;
+    }
+    return results
+}
+console.log(exponentiate(3,4))
