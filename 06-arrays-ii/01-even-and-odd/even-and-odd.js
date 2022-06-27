e4f7c21306cd332a63a70547abf7410c302170af
Amani Mkamba <amani.mkamba@gmail.com>
2022-06-27 08:24:46 -0400

    evenAndOdd

--- 06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ 06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+function evenAndOdd(arrNums) {
+    let evenNums = [];
+    let oddNums = [];
+    let newArr = [evenNums, oddNums];
+
+    for( let i = 0; i < arrNums.length; i++) {
+        let currentNums = arrNums[i]
+        if(currentNums % 2 === 0) {
+            evenNums.push(currentNums);
+        }
+        else {
+            oddNums.push(currentNums)
+        }
+    }
+    return newArr;
+}
+
+console.log(evenAndOdd([1, 2, 3, 4, 5, 6]))
