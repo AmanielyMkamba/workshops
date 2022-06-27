d7c9673e36c829551b0ca38bd3b020d782c1f29b
Amani Mkamba <amani.mkamba@gmail.com>
2022-06-27 07:20:55 -0400

    odd-couple

--- 05-arrays-i/01-odd-couple/odd-couple.js
+++ 05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+function oddCouple(arr) {
+    let newArr= [];
+    if(arr%2===0) {
+        return [];
+    }
+    for(let i = 0; i <= arr.length-1; i++) {
+        if(arr[i]%2===1) {
+            newArr.push(arr[i])
+        }
+        if(newArr.length === 2) {
+            return newArr
+        }
+    }
+    return newArr
+}
+
+console.log(oddCouple([10, 15, 20]))
+console.log(oddCouple([1, 2, 3, 4, 5]))
+console.log(oddCouple(2, 4, 6, 8))
